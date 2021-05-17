497499f1937fcccded0da8eb4d85fab558bc99fb
Hilder Arrieta <hbarrieta25@gmail.com>
2021-05-16 23:52:21 -0500

    Test

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import Phrase from "./Phrase"
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <Phrase/>
+        </main>
+    )
+}
+
+export default App
